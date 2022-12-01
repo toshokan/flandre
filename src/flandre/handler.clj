8537c8538ad1a9356370bff20213af77983f8d85
toshokan <toshokan@shojigate.net>
2022-11-30 21:27:30 -0500

    feat: multipart file uploads

--- src/flandre/handler.clj
+++ src/flandre/handler.clj
@@ -1,8 +1,8 @@
 (ns flandre.handler
   (:require [reitit.ring :as ring]
+            [ring.middleware.resource :as res]
             [ring.middleware.json :refer [wrap-json-response]]
-            [flandre.uploads :as uploads])
-  (:import [java.io File]))
+            [flandre.uploads :as uploads]))
 
 (defn inject-depends [depends]
   (fn [handler]
@@ -12,9 +12,10 @@
 (defn router [db cfg]
   (ring/ring-handler
    (ring/router
-    [["/f/:tag" {:get uploads/get-file-handler
+    [["/api"
+      ["/f/:tag" {:get uploads/get-file-handler
                  :delete uploads/delete-file-handler}]
-     ["/f" {:post uploads/upload-file-handler}]])
-   nil
+      ["/f" {:post uploads/upload-file-handler}]]])
+   (ring/create-resource-handler {:path "/"})
    {:middleware [(inject-depends {:db db :cfg cfg})
                  wrap-json-response]}))
--- src/flandre/uploads.clj
+++ src/flandre/uploads.clj
@@ -1,7 +1,9 @@
 (ns flandre.uploads
-  (:require [next.jdbc :as jdbc]
+  (:require [clojure.string :as s]
+            [next.jdbc :as jdbc]
             [honey.sql :as sql]
             [ring.util.response :as r]
+            [ring.middleware.multipart-params :as mp]
             [flandre.limits :as limits]
             [flandre.queries :as queries]
             [flandre.files :as files]
@@ -56,21 +58,39 @@
         query (partial queries/count-files-in-last db)]
     (limits/past-rate-limit? cfg uploader query)))
 
-(defn- upload-file [req]
-  (let [db (:db req)
-        cfg (:cfg req)
-        files-root (:files-root cfg)
-        expiry-time (:expiry-time cfg)
-        tag (core/create-tag)
+(defn- store-file [db root expires-in name uploader stream]
+  (let [tag (core/create-tag)
         delete-token (core/create-delete-token)]
-    (files/upload-file tag (:body req) files-root)
-    (let [expiry (queries/insert-file-info
-                  db tag tag (:remote-addr req) 30 (:digest delete-token))]
-      (-> (r/response {:url (str (:base-url cfg) "/f/" tag)
-                       :expiry expiry
-                       :delete-token (:token delete-token)})))))
+    (files/upload-file tag stream root)
+    (let [expiry (queries/insert-file-info db tag name uploader expires-in (:digest delete-token))]
+      {:filename name
+       :tag tag
+       :expiry expiry
+       :delete-token (:token delete-token)})))
+
+(defn- make-store-fn [req]
+  (let [db (:db req)
+        cfg (:cfg req)]
+    (fn [{:keys [filename content-type stream]}]
+      (store-file db
+                  (:files-root cfg)
+                  (:expiry-time cfg)
+                  filename
+                  (:remote-addr req)
+                  stream))))
+
+(defn- upload-files [req]
+  (let [store-fn (make-store-fn req)
+        req (mp/multipart-params-request req {:store store-fn})
+        files (vals (:multipart-params req))
+        file-count (count files)]
+    (cond
+      (empty? files) (-> (r/response "bad request")
+                         (r/status 400))
+      (= 1 file-count) (r/response (nth files 0))
+      :else (r/response files))))
 
 (defn upload-file-handler [req]
   (if (past-rate-limit? req)
     (rate-limit-exceeded)
-    (upload-file req)))
+    (upload-files req)))
